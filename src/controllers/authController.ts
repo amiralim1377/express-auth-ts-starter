f1d962a333ed35e7e1bfb68bbcbf2a1a999851ec
AMIRALI <alimoradi7718@gmail.com>
2026-07-23 19:50:29 +0330

    feat: add authorization middleware and update user routes for role-based access control

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -97,3 +97,16 @@ export const protect = async (
 
   next();
 };
+
+export const restrictTo = (...roles: string[]) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    // roles ['admin', 'user']. role='user'
+    if (!roles.includes(req.user?.role as string)) {
+      return next(
+        new AppError("You do not have permission to perform this action", 403),
+      );
+    }
+
+    next();
+  };
+};
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,12 +1,17 @@
 import express from "express";
-import { login, protect, signUp } from "../controllers/authController";
+import {
+  login,
+  protect,
+  restrictTo,
+  signUp,
+} from "../controllers/authController";
 
 const router = express.Router();
 
 router.post("/signup", signUp);
 router.post("/login", login);
 
-router.get("/test-protect", protect, (req, res) => {
+router.get("/test-protect", protect, restrictTo("admin"), (req, res) => {
   res.status(200).json({ message: "شما با موفقیت از بادیگارد رد شدید!" });
 });
 
