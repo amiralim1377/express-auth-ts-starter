0bb14652e5a80249a61bcf9e2901439465df01aa
AMIRALI <alimoradi7718@gmail.com>
2026-07-25 11:26:49 +0330

    feat: add express-rate-limit middleware to enhance API rate limiting

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import { config } from "./config/env";
 import morgan from "morgan";
 import { AppError } from "./utils/AppError";
 import { globalErrorHandler } from "./middlewares/errorHandler";
+import rateLimit, { MINUTE } from "express-rate-limit";
 
 const app: Express = express();
 
@@ -11,7 +12,23 @@ if (config.nodeEnv === "development") {
   app.use(morgan("dev"));
 }
 
-app.use(express.json());
+app.set("trust proxy", 1);
+
+if (config.nodeEnv === "development") {
+  app.use(morgan("dev"));
+}
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 100,
+  standardHeaders: "draft-8",
+  legacyHeaders: false,
+  message: "Too many requests from this IP, please try again later.",
+});
+
+app.use("/api", limiter);
+
+app.use(express.json({ limit: "10kb" }));
 
 app.use("/api/v2/users", userRouter);
 
