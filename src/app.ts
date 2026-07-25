8ba58f5335227819367467d8a0c32080f14a95c7
AMIRALI <alimoradi7718@gmail.com>
2026-07-25 12:05:07 +0330

    feat: add hpp middleware to prevent parameter pollution and enhance security

--- src/app.ts
+++ src/app.ts
@@ -6,24 +6,24 @@ import { AppError } from "./utils/AppError";
 import { globalErrorHandler } from "./middlewares/errorHandler";
 import rateLimit from "express-rate-limit";
 import helmet from "helmet";
-import { xss } from "express-xss-sanitizer";
 import mongoSanitize from "express-mongo-sanitize";
+import { xss } from "express-xss-sanitizer";
+import hpp from "hpp";
 
 const app: Express = express();
 
-//set security http header
+// 1) Set security HTTP headers
 app.use(helmet());
 
-if (config.nodeEnv === "development") {
-  app.use(morgan("dev"));
-}
-
+// 2) Trust proxy for production environment
 app.set("trust proxy", 1);
 
+// 3) Development logging
 if (config.nodeEnv === "development") {
   app.use(morgan("dev"));
 }
 
+// 4) Rate Limiting
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   limit: 100,
@@ -33,20 +33,38 @@ const limiter = rateLimit({
 });
 app.use("/api", limiter);
 
+// 5) Body parser, reading data from body into req.body
 app.use(express.json({ limit: "10kb" }));
 
-// 1) Data sanitization against NoSQL query injection
+// 6) Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
 
-// 2) Data sanitization against XSS
+// 7) Data sanitization against XSS
 app.use(xss());
 
+// 8) Prevent parameter pollution (MUST be before routes)
+app.use(
+  hpp({
+    whitelist: [
+      "duration",
+      "ratingsQuantity",
+      "ratingsAverage",
+      "maxGroupSize",
+      "difficulty",
+      "price",
+    ],
+  }),
+);
+
+// 9) Routes
 app.use("/api/v2/users", userRouter);
 
-app.all("/{*splat}", (req, res, next) => {
-  next(new AppError(`Cannot find ${req.originalUrl}`, 400));
+// 10) Handle Unhandled Routes
+app.all("*", (req, res, next) => {
+  next(new AppError(`Cannot find ${req.originalUrl} on this server!`, 404));
 });
 
+// 11) Global Error Handler
 app.use(globalErrorHandler);
 
 export default app;
